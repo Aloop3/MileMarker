01467e1ed0a8e0e4f7034fc6eb5fc3aec2b4f825
sharontoto <sharontototo@gmail.com>
2026-05-02 09:04:08 -0700

    feat: create vehicle addition form (#4)

--- ios/MileMarker/MileMarker/Tabs/Vehicles/VehicleEntryView.swift
+++ ios/MileMarker/MileMarker/Tabs/Vehicles/VehicleEntryView.swift
@@ -6,18 +6,64 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct VehicleEntryView: View {
+    @State private var make = ""
+    @State private var model = ""
+    @State private var nickname = ""
+    @State private var year: Int = Calendar.current.component(.year, from: Date())
+    @State private var odometer = ""
+    
     @Environment(\.dismiss) private var dismiss
+    @Environment(DataContainer.self) private var dataContainer
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            contentStack
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Add a new vehicle")
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button("Cancel", systemImage: "xmark") {
                         dismiss()
                     }
                 }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save", systemImage: "checkmark") {
+                        let newVehicle = Vehicle(
+                            nickname: nickname,
+                            year: String(year),
+                            make: make,
+                            model: model,
+                            lastOdometer: Int(odometer) ?? 0
+                        )
+                        dataContainer.context.insert(newVehicle)
+                        do {
+                            try dataContainer.context.save()
+                            dismiss()
+                        } catch {
+                            // don't dismiss
+                        }
+                    }
+                    .disabled(make.isEmpty || model.isEmpty || odometer.isEmpty)
+                }
+            }
+        }
+    }
+
+    var contentStack: some View {
+        Form {
+            Section(header: Text("Vehicle Details")) {
+                TextField("Nickname", text: $nickname)
+                TextField("Make", text: $make)
+                TextField("Model", text: $model)
+                Picker("Year", selection: $year) {
+                    ForEach((1980...Calendar.current.component(.year, from: Date())+1).reversed(), id: \.self) { year in
+                        Text(String(year)).tag(year)
+                    }
+                }
+                TextField("Last Odometer", text: $odometer)
+                    .keyboardType(.numberPad)
             }
         }
     }
@@ -25,4 +71,5 @@ struct VehicleEntryView: View {
 
 #Preview {
     VehicleEntryView()
+        .sampleDataContainer()
 }
